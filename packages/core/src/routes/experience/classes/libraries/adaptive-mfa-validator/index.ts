3a2775c3b26403c7e7b5b0af0bb63b658432c0d9
odinwerks <odinwerks@users.noreply.github.com>
2026-05-13 12:57:32 +0400

    refactor: robust session device extractor, always builds string from UA
    Removes the CH-header gate that blocked device string generation for browsers that don't send sec-ch-ua-model. Now always builds a device string from UA parsing when user agent is present, enhanced by client hints when available. Corrects desktop-mode spoofed Linux claims to Android via sec-ch-ua-platform. All parsing is wrapped in trySafe to never crash sign-in.

--- packages/core/src/routes/experience/classes/libraries/adaptive-mfa-validator/index.ts
+++ packages/core/src/routes/experience/classes/libraries/adaptive-mfa-validator/index.ts
@@ -114,17 +114,33 @@ export class AdaptiveMfaValidator {
       ...conditional(userAgent && { userAgent }),
       ...getInjectedHeaderValues(headers),
       ...conditional(
-        headers['sec-ch-ua-model'] && {
+        userAgent && {
           device: trySafe(() => {
-            const parser = new UAParser(userAgent ?? '', undefined, {
+            const chHeaders = {
               'sec-ch-ua-model': headers['sec-ch-ua-model'],
               'sec-ch-ua-platform-version': headers['sec-ch-ua-platform-version'],
               'sec-ch-ua-platform': headers['sec-ch-ua-platform'],
               'sec-ch-ua-full-version-list': headers['sec-ch-ua-full-version-list'],
-            });
-            const { browser, os, device } = parser.getResult().withClientHints();
-            const deviceModel = [device.vendor, device.model].filter(Boolean).join(' ');
-            return [browser.name, os.name, deviceModel].filter(Boolean).join(' on ');
+              'sec-ch-ua-mobile': headers['sec-ch-ua-mobile'],
+            };
+            const hasCh = Object.values(chHeaders).some(Boolean);
+            const parser = new UAParser(userAgent, undefined, hasCh ? chHeaders : undefined);
+            const { browser, os, device } = hasCh
+              ? parser.getResult().withClientHints()
+              : parser.getResult();
+
+            // Desktop-mode spoofed UAs claim "Linux" when CH platform says otherwise
+            const platformName =
+              os.name === 'Linux' &&
+              headers['sec-ch-ua-platform'] === '"Android"'
+                ? 'Android'
+                : os.name;
+
+            const model = [device.vendor, device.model]
+              .filter(Boolean)
+              .join(' ') || undefined;
+
+            return [browser.name, model || platformName].filter(Boolean).join(' on ');
           }),
         }
       ),
